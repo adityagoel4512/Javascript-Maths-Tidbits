a8a591908ff77f35297a33dd5eafbf305f8cc1da
Adi <agoel4512@gmail.com>
2019-07-15 13:58:20 +0100

    removed jquery

--- PCA-Visualiser/script/pca.js
+++ PCA-Visualiser/script/pca.js
@@ -162,12 +162,16 @@ var extra_text_p1 = false;
 var extra_text_p3 = false;
 var f_index = [];
 
-function pcaIntro(type) {
+function pcaIntro() {
     Plotly.purge("graph1");
-    document.getElementById('information').innerText = "\n\nAn introduction to Principle Component Analysis.";
+    document.getElementById('information').innerText = "\n\nAn introduction to Principle Component Analysis. \n\n If there's any preamble we can put it here, otherwise this landing page can be removed.";
     
     f_index.push(flower_set.findIndex(vector => vector[vector.length-1] == versicolor));
     f_index.push(flower_set.findIndex(vector => vector[vector.length-1] == virginica));
+
+    flower_set.forEach(vector => {
+        vector.pop();
+    });
 
 }
 
@@ -721,36 +725,4 @@ function switchTab(tabName) {
 
 }
   
-
-function main() {
-    /*Jquery*/ //NB: Put Jquery stuff in the main not in HTML
-    
-    $("input[type=range]").each(function () {
-        var displayEl;
-        /*Allows for live update for display values*/
-        $(this).on('input', function(){
-            //Displays: (FLT Value) + (Corresponding Unit(if defined))
-            $("#"+$(this).attr("id") + "Display").text( $(this).val() + $("#"+$(this).attr("id") + "Display").attr("data-unit") );
-            // updatePlot(); //Updating the plot is linked with display (Just My preference)
-        
-        });
-
-    });
-
-    /*Tabs*/
-    $(function() {
-        $('ul.tab-nav li a.button').click(function() {
-            var href = $(this).attr('href');
-            $('li a.active.button', $(this).parent().parent()).removeClass('active');
-            $(this).addClass('active');
-            $('.tab-pane.active', $(href).parent()).removeClass('active');
-            $(href).addClass('active');
-
-            pcaIntro(href); //re-initialise when tab is changed
-            return false;
-        });
-    });
-
-    pcaIntro("#basis");
-}
-$(document).ready(main); //Load main when document is ready.
+$(document).ready(pcaIntro); 
